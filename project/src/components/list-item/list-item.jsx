d19b3cfc0041fa7ea43b85a3dc799466c216d2ef
Mollycodd1e <selik7@mail.ru>
2021-10-28 17:46:27 +0300

    Добавляет чекбокс в React

--- project/src/components/list-item/list-item.jsx
+++ project/src/components/list-item/list-item.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {deleteTask} from '../store/action';
-import { useDispatch } from 'react-redux';
+import {useDispatch} from 'react-redux';
 
 function ListItem(props) {
+
+  const hiddenButton = 'list__button--closed';
 
   const {task, index} = props;
 
   const dispatch = useDispatch();
 
+  const [check, setCheck] = useState(false);
+
   const deleteNewTask = (task, index) => {
     dispatch(deleteTask(task, index));
   }
 
+  const showButton = (evt) => {
+    const buttons = evt.closest('li').querySelectorAll('button');
+    buttons.forEach((element) => {
+      if (element.classList.contains(hiddenButton)) {
+        element.classList.remove(hiddenButton);
+      }
+    })
+  }
+
+  const hideButton = (evt) => {
+    const buttons = evt.closest('li').querySelectorAll('button');
+    buttons.forEach((element) => {
+      if (!element.classList.contains(hiddenButton)) {
+        element.classList.add(hiddenButton);
+      }
+    })
+  }
+
   return (
-    <li>
-      <span>{index + 1}.</span>
-      <p>{task}</p>
+    <li onMouseEnter={(evt) => showButton(evt.target)} onMouseLeave={(evt) => hideButton(evt.target)}>
       <div className="list__check-wrapper">
-        <label className="visually-hidden" forhtml="id-check-task">Выполненная задача</label>
-        <input id="check-task" type="checkbox" />
+        <input className="visually-hidden" name="check-task" id="check-task" type="checkbox" checked={check === false  ? false : true}/>
+        <label forhtml="check-task" onClick={() => {check === true ? setCheck(false) : setCheck(true)}}><span>{index + 1}.</span>{task}</label>
       </div>
-      <button className="list__delete-task" onClick={() => deleteNewTask(task, index)}>Удалить задачу</button>
+      <button className="list__button list__button--edit list__button--closed" aria-label="Редактировать задачу"></button>
+      <button className="list__button list__button--trash list__button--closed" aria-label="Удалить задачу" onClick={() => deleteNewTask(task, index)}></button>
     </li>
   )
 }
--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -26,10 +26,14 @@ function Main() {
       <section className="main-page__wrapper">
         <h1>ToDoList</h1>
         <div className="main-page__add-task">
-          <label className="visually-hidden" forhtml="id-new-task">Добавить задачу</label>
-          <input type="text" id="new-task" name="new-task" onChange={(evt) => evt.target.value}/>
-          <button className="main-page__new-task-btn" onClick={() => addNewTask(document.querySelector('input[name="new-task"]').value)}></button>
-          <button className="main-page__clear-task-btn" onClick={() => clearInput(document.querySelector('input[name="new-task"]'))}>Очистить</button>
+          <input name="new-task" type="text" id="new-task" />
+          <label className="visually-hidden" forhtml="new-task">Добавить задачу</label>
+          <button className="main-page__new-task-btn" onClick={() => addNewTask(document.querySelector('input[name="new-task"]').value)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+          </button>
+          <button className="main-page__clear-task-btn" onClick={() => clearInput(document.querySelector('input[name="new-task"]'))}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+          </button>
         </div>
         <List tasksList={tasksList} />
       </section>
